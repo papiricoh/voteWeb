43b9208e320941819c1e49c5de8ecbc95de8017c
papiricoh <slendeman0200@gmail.com>
2024-11-20 20:19:18 +0100

    Add VoteChart component and integrate it into HomeView

---
+++ src/components/charts/VoteChart.vue
@@ -0,0 +1,95 @@
+
+<script>
+    export default {
+        data() {
+            return {
+                
+            };
+        },
+        props: {
+            total: {
+                type: Number,
+                required: true,
+            },
+            favour: {
+                type: Number,
+                required: true,
+            },
+            against: {
+                type: Number,
+                required: true,
+            },
+        },
+        computed: {
+            favourPercentage() {
+                return (this.favour / this.total) * 100;
+            },
+            againstPercentage() {
+                return (this.against / this.total) * 100;
+            },
+        },
+    };
+</script>
+
+<template>
+    <div class="v_cont">
+        <div class="vote_container">
+            <div class="favour_votes" :style="'width: ' + favourPercentage + '%;'"></div>
+            <div class="against_votes" :style="'width: ' + againstPercentage + '%;'"></div>
+        </div>
+        <div class="vote_data_c">
+            <span>{{ favour }}</span>
+
+            <span>{{ against }}</span>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.v_cont {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: .6rem;
+    width: 100%;
+}
+
+.vote_container {
+    display: flex;
+    box-sizing: border-box;
+    justify-content: space-between;
+    background-color: rgba(128, 128, 128, 0.302);
+    height: 1.4rem;
+    width: 80%;
+    color: var(--white);
+}
+
+.favour_votes {
+    background-color: green;
+    height: 100%;
+    width: 50%;
+    transition: .4s;
+}
+.against_votes {
+    background-color: darkred;
+    height: 100%;
+    width: 50%;
+    transition: .4s;
+}
+.vote_data_c {
+    display: flex;
+    justify-content: space-between;
+    width: 80%;
+    font-size: .8rem;
+    font-weight: bold;
+}
+
+.vote_data_c span:first-child {
+    color: green;
+}
+
+.vote_data_c span:last-child {
+    color: darkred;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,26 @@
 <script setup>
 import ParliamentChart from '@/components/charts/ParliamentChart.vue';
+import VoteChart from '@/components/charts/VoteChart.vue';
+</script>
+
+<script>
+    export default {
+        data() {
+            return {
+                
+            };
+        },
+        computed: {
+          
+        },
+    };
 </script>
 
 <template>
   <main>
+    <div>Parliament</div>
     <ParliamentChart></ParliamentChart>
+    <VoteChart :total="50" :favour="20" :against="10"></VoteChart>
   </main>
 </template>
 
@@ -12,5 +28,9 @@ import ParliamentChart from '@/components/charts/ParliamentChart.vue';
 main {
   padding: 1rem;
   box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  align-items: center;
 }
 </style>
